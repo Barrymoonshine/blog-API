9841b63ec16797847ca040309215a658f268b73c
Nick Sheward <nickshwrd@gmail.com>
2023-08-21 15:52:05 +0100

    Securely connected to DB

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbURI = `mongodb+srv://${process.env.DB_CREDENTIALS}@cluster0.wym9xjg.mongodb.net/blog-API?retryWrites=true&w=majority`;
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    app.listen(3000, '0.0.0.0', () => {
+      console.log('Server is listening on port 3000');
+    });
+  } catch (err) {
+    console.log(`Mongoose connection error: ${err}`);
+  }
+};
+connectToDb();
