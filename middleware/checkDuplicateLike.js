8d3b20143b916cd6f8bc9fa6ac0e3915d4ebbf7d
Nick Sheward <nickshwrd@gmail.com>
2023-09-08 15:53:22 +0100

    Updated isDocLiked logic in checkDuplicateLike

--- middleware/checkDuplicateLike.js
+++ middleware/checkDuplicateLike.js
@@ -1,9 +1,9 @@
 import Like from '../models/like.js';
 
 const checkDuplicateLike = async (req, res, next) => {
-  const { docID } = req.body;
+  const { docID, username } = req.body;
 
-  const isDocLiked = await Like.findOne({ docID });
+  const isDocLiked = await Like.findOne({ docID, username });
 
   if (isDocLiked) {
     return res.status(400).json({ error: 'Already liked' });
