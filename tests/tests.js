c776c0ce2ee862c78412853617bb83432f5aa824
Nick Sheward <nickshwrd@gmail.com>
2023-10-26 15:49:30 +0100

    Created user route test block and passed first log in test

--- tests/tests.js
+++ tests/tests.js
@@ -2,16 +2,20 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import express from 'express';
 import blogRoutes from '../routes/blogRoutes.js';
+import userRoutes from '../routes/userRoutes.js';
 import initMongoServer from '../mongoConfigs/mongoTestConfig.js';
 import Blog from '../models/blog.js';
-import { createToken } from '../helpers/helpers.js';
+import User from '../models/user.js';
+import { createToken, hashPassword } from '../helpers/helpers.js';
 import 'dotenv/config';
 
 const app = express();
 const should = chai.should();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use('/blogs', blogRoutes);
+app.use('/user', userRoutes);
 
 chai.use(chaiHttp);
 
@@ -44,22 +48,32 @@ const createTestBlogs = async () => {
   return [blogA, blogB];
 };
 
+const createTestUser = async () => {
+  const hashedPassword = await hashPassword('Password!234');
+  const testUser = new User({ username: 'Test', password: hashedPassword });
+  return testUser;
+};
+
+// Global variable used for blogA and B use in blog test block, note: refactor to restrict access
+let blogA;
+let blogB;
+
+// Tests set up
+before(async () => {
+  await initMongoServer();
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+
+  [blogA, blogB] = await createTestBlogs();
+  const testUser = await createTestUser();
+
+  await Promise.all([blogA.save(), blogB.save(), testUser.save()]);
+});
+
 describe('Blog route tests', () => {
-  let blogA;
-  let blogB;
   // Create token with test id value
   const token = createToken(1);
-  // Tests set up
-  before(async () => {
-    await initMongoServer();
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-
-    [blogA, blogB] = await createTestBlogs();
-
-    await Promise.all([blogA.save(), blogB.save()]);
-  });
 
   it('should return a list of blogs ordered by latest first when calling GET /blogs', async () => {
     const res = await chai.request(app).get('/blogs');
@@ -78,7 +92,7 @@ describe('Blog route tests', () => {
     res.body.should.have.property('title', 'Test blog A');
   });
 
-  it('should return a string confirming blog published updated when calling PATCH /blogs', async () => {
+  it('should return a string confirming published property updated when calling PATCH /blogs', async () => {
     const res = await chai
       .request(app)
       .patch('/blogs')
@@ -87,5 +101,18 @@ describe('Blog route tests', () => {
 
     res.should.have.status(200);
     res.text.should.deep.include('Blog published updated');
+  });
+});
+
+describe('User route tests', () => {
+  it('should log valid user in and return a token when calling POST /user', async () => {
+    const res = await chai
+      .request(app)
+      .post('/user')
+      .set({ 'content-type': 'application/json' })
+      .send({ username: 'Test', password: 'Password!234' });
+
+    res.should.have.status(200);
+    res.body.should.have.property('token');
   });
 });
